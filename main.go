fef60a2c32da287bdd190904776bfeb92f6119e9
RatedRR <4971331+mhm233@users.noreply.github.com>
2024-02-11 20:48:37 +0330

    Update main.go

--- main.go
+++ main.go
@@ -109,11 +109,11 @@ func deployTx(pub string, a *account.Account) {
 		resp, err := a.AddDeployAccountTransaction(context.Background(), rpc.BroadcastDeployAccountTxn{DeployAccountTxn: tx})
 		if err != nil {
 			fmt.Println(err)
-			time.Sleep(6 * time.Second)
+			time.Sleep(41 * time.Second)
 			continue
 		}
 		fmt.Println("deployd: ", resp.ContractAddress)
-		time.Sleep(3 * time.Second)
+		time.Sleep(21 * time.Second)
 
 	}
 
